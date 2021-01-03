58b33b776c02bcfa4ec2c67aadbcd7ebae4f1a20
flobev <florian.bevierre@gmail.com>
2021-01-03 17:56:02 +0100

    Modification constructeur

--- src/models/Utilisateur.ts
+++ src/models/Utilisateur.ts
@@ -1,48 +1,48 @@
 /* import MySQL from '../db/MySQL'; */
 export default class Utilisateur {
 
-    protected id_utilisateur?: number | null;
-    private nom: string;
-    private prenom: string;
-    private email: string;
-    private mdp: string;
-    private sexe: string;
-    private date_naissance: string;
-    private role: string | null;
-    private creation: string | null;
-    private modification: string | null;
-    private abonnement: string | null;
+    protected id_utilisateur ? : number | null;
+    public nom: string | null;
+    public prenom: string | null;
+    public email: string | null;
+    public mdp: string | null;
+    public sexe: string | null;
+    public date_naissance: string | null;
+    public role?: string;
+    public creation?: string;
+    public modification?: string;
+    public abonnement?: string;
 
     protected table: string = 'utilisateur';
 
     /**
      * Creates an instance of Utilisateur.
      * @param {(Utilisateur(instance) | null)} id
-     * @param {string} [nom]
-     * @param {string} [prenom]
-     * @param {string} [email]
-     * @param {string} [mdp]
-     * @param {string} [sexe]
-     * @param {string} [date_naissance]
-     * @param {string} [role='']
-     * @param {string} [creation='']
-     * @param {string} [modification='']
-     * @param {string} [abonnement='']
+     * @param {string} [nom='']
+     * @param {string} [prenom='']
+     * @param {string} [email='']
+     * @param {string} [mdp='']
+     * @param {string} [sexe='']
+     * @param {string} [date_naissance='']
+     * @param {string} [role]
+     * @param {string} [creation]
+     * @param {string} [modification]
+     * @param {string} [abonnement]
      * @memberof Utilisateur
      */
 
-    constructor(utilisateur: Utilisateur | null, nom: string, prenom: string, email: string, mdp: string, sexe: string, date_naissance: string, role: string = '', creation: string = '', modification: string = '', abonnement: string = ''){
+    constructor(utilisateur: Utilisateur | null, l_name: string = '', f_name: string = '', mail: string = '', password: string = '', sexe: string = '', date_birth: string = '', role ? : string, created_at ? : string, update_at ? : string, sub ? : string){
         if (utilisateur === null) {
-            this.nom = nom.toUpperCase().trim();;
-            this.prenom = prenom.toLowerCase().trim();
-            this.email = email;
-            this.mdp = mdp;
+            this.nom = l_name.toUpperCase().trim();;
+            this.prenom = f_name.toLowerCase().trim();
+            this.email = mail;
+            this.mdp = password;
             this.sexe = sexe;
-            this.date_naissance = date_naissance;
-            this.role = (role === undefined) ? null : role;
-            this.creation = (creation === undefined) ? null : creation;
-            this.modification = (modification === undefined) ? null : modification;
-            this.abonnement = (abonnement === undefined) ? null : abonnement;
+            this.date_naissance = date_birth;
+            this.role = role;
+            this.creation = created_at;
+            this.modification = update_at;
+            this.abonnement = sub;
         } else {
             this.nom = utilisateur.nom;
             this.prenom = utilisateur.prenom;
@@ -50,62 +50,64 @@ export default class Utilisateur {
             this.mdp = utilisateur.mdp;
             this.sexe = utilisateur.sexe;
             this.date_naissance = utilisateur.date_naissance;
-            this.role = (utilisateur.role === undefined) ? null : utilisateur.role;
-            this.creation = (utilisateur.creation === undefined) ? null : utilisateur.creation;
-            this.modification = (utilisateur.modification === undefined) ? null : utilisateur.modification;
-            this.abonnement = (utilisateur.abonnement === undefined) ? null : utilisateur.abonnement;
+            this.role = utilisateur.role;
+            this.creation = utilisateur.creation;
+            this.modification = utilisateur.modification;
+            this.abonnement = utilisateur.abonnement;
             this.id_utilisateur = utilisateur.id;
         }
     }
 
+    /************************* GETTER *************************/
+
     get id(): number {
-        return <number > this.id_utilisateur;
+        return <number> this.id_utilisateur;
     }
 
     get lastName(): string {
-        return this.nom;
+        return <string> this.nom;
     }
 
     get firtsName(): string {
-        return this.prenom;
+        return <string> this.prenom;
     }
 
     get mail(): string {
-        return this.email;
+        return <string> this.email;
     }
 
     get password(): string {
-        return this.mdp;
+        return <string> this.mdp;
     }
 
     get gender(): string {
-        return this.sexe;
+        return <string> this.sexe;
     }
 
     get birth(): string {
-        return this.date_naissance;
+        return <string> this.date_naissance;
     }
 
     get assignement(): string {
-        return (this.role === null) ? '' : this.role;
+        return <string> this.role;
     }
 
     get createAt(): string {
-        return (this.creation === null) ? '' : this.creation;
+        return <string> this.creation;
     }
 
     get updateAt(): string {
-        return (this.nom === null) ? '' : this.nom;
+        return <string> this.nom;
     }
 
     get subscription(): string {
-        return (this.nom === null) ? '' : this.nom;
+        return <string> this.nom;
     }
 
 
-    get attributInsert(): Array < string > {
+    /* get attributInsert(): Array < string > {
         return ['nom', 'prenom', 'email', 'mdp', 'sexe', 'role', 'date_naissance', 'creation', 'modification', 'abonnement']
-    }
+    } */
 
 
     /**
