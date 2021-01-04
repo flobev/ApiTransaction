a08bace61fcee402f95ce2543144396ba2e87302
flobev <florian.bevierre@gmail.com>
2021-01-04 20:41:37 +0100

    Ajout fichier interface Carte

---
+++ src/interfaces/Carte.ts
@@ -0,0 +1,10 @@
+export interface CarteInterfaces {
+
+    utilisateur_id_utilisateur: number | null | undefined;
+    id_carte: number;
+    numero_carte: string;
+    mois: string;
+    annee: string;
+    defaut: string;
+    save(): Promise < number >
+}
