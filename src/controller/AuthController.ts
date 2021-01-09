23548795595164edfc54af950eb873fe7a1360cd
flobev <florian.bevierre@gmail.com>
2021-01-09 15:22:06 +0100

    Ajout methode authentification

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -67,6 +67,32 @@ export class AuthController {
         }
     }
 
+    static authentification = async(req: Request, res: Response) => {
+        let data: any = req.body;
+
+        try {
+            if (await Tuteur.isExiste(data.email))
+                throw new Error(`Email exist!`)
+
+            const utilisateur = new Utilisateur(null, data.prenom, data.nom, data.dateNaiss, data.pays, data.adresse, data.ville, data.zipcode);
+            await utilisateur.save();
+            const pass = await PasswordException.hashPassword(data.password);
+            const tuteur = new Tuteur(utilisateur, data.email, pass);
+            await tuteur.save();
+
+            const theToken: any = await sign({ id: tuteur.utilisateur, name: tuteur.fullname }, < string > process.env.JWT_KEY, { expiresIn: '1m' })
+
+            const token = {
+                token: theToken,
+                expired: await ( < any > decode(theToken)).exp
+            }
+            return res.status(201).json(token);
+
+        } catch (err) {
+            return res.status(401).json({ error: true, message: err.message }).end();
+        }
+    }
+
     refreshToken = async(req: Request, res: Response) => {}
     checkToken = async(req: Request, res: Response) => {}
     logout = async(req: Request, res: Response) => {}
